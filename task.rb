8501398d2ab5476067dbde7a4797b04211549b61
並木　久稔 <namikihisatoshi@namikihisatoshinoMacBook-Pro.local>
2020-11-27 22:58:30 +0900

    q17~19

--- task.rb
+++ task.rb
@@ -150,7 +150,19 @@ end
 
 class UserQ17
   # 以下に回答を記載
+    def initialize(name:,age:,gender:)
+      @name = name
+      @age = age
+      @gender = gender
+    end
 
+    def info
+      puts <<~TEXT
+      "名前:#{@name}"
+      "年齢:#{@age}"
+      "性別:#{@gender}"
+      TEXT
+    end
 end
 
 def q17
@@ -165,7 +177,18 @@ end
 
 class UserQ18
   # 以下に回答を記載
+  def initialize(name:, age:)
+    @name = name
+    @age = age
+  end
 
+  def introduce
+    if @age == 32
+      puts "こんにちは、#{@name}と申します。宜しくお願いいたします。"
+    else
+      puts "はいさいまいど〜、#{@name}です！！！"
+    end
+  end
 end
 
 def q18
@@ -179,8 +202,8 @@ end
 
 class Item
   # 以下を修正して下さい
-
-  def initialize(name)
+  attr_accessor :name
+  def initialize(name:)
     @name = name
   end
 end
@@ -193,7 +216,7 @@ end
 
 class UserQ20
   # 以下に回答を記載
-
+  
 end
 
 class Zoo
